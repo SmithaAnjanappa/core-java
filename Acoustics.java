0a13239027038f259d9a6952f0d8679ebf86cfe6
Smitha <smitha.ag321@gmail.com>
2025-06-17 14:05:35 +0530

    sop

---
+++ Acoustics.java
@@ -0,0 +1,14 @@
+class Acoustics{
+public static void main(String bou[]){
+System.out.println("Stereo Headset: Padded earcup and headband with in-line control on cable for easy access to control audio volume. Unidirectional microphone minimizes unwanted noise. Microphone only picks up sound from a specific side or direction of the microphone, minimizing ambient noise while speaking or recording. Perfect for K12 classrooms or home.\n");
+
+System.out.println("Universal Fit: With an adjustable microphone boom, headband and leatherette earpads, this headset can be adjusted to fit most head sizes. Designed to be worn with the microphone boom on the left- or right-hand side.\n");
+
+System.out.println("Durable Design: Adjustable headband with easy to clean leatherette earpads. Headset and tangle free braided TuffCord cable are designed for hard wearing and to be long lasting.\n");
+
+System.out.println("Easy to Use: Simply plug in and play, no installation needed. Connect 3.5mm cable directly to your Mac, PC or any electronic device with a 3.5mm audio jack connection.\n");
+
+System.out.println("Includes: Cyber Acoustics AC-5002 Analog Headset and User Guide. Includes 1 year factory warranty.");
+
+}
+}
